02cea48895b1786538e0c38d42e762a1efde08e1
Piko <pgqirana@gmail.com>
2017-06-17 11:22:49 +0200

    First ChilliFans commit

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,46 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+$(document).ready(function() { 
+	// jQuery is properly loaded at this point
+	// so proceed to bind the Cordova's deviceready event
+	//app.readPosts();
+	$(document).bind('deviceready', app.onDeviceReady); 
+});
+
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+	posts_url: "http://localhost:3000/posts/",
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+	onDeviceReady: function() {
+		console.log('Device is ready');
+		app.readPosts();
+	},
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	readPosts: function() {
+		console.log('Reading posts');
+		$.ajax({
+			type: "GET",
+			dataType: "json",
+			url: app.posts_url,
+			success: app.onSuccess,
+			error: app.onError
+		});
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		console.log('Reading posts asynchrounously');
+	},
 
-        console.log('Received Event: ' + id);
-    }
+	onSuccess: function(data) {
+		var items = [];
+		$.each(data, function(key, val){
+			items.push('<a href="' + app.posts_url + val.id + '">' + val.id + ' - ' +val.title + '</a>');
+		});
+		$('#posts').html(items.join('<br/>'));
+		console.log('Exiting onSuccess');
+	},
+
+	onError: function(data, textStatus, errorThrown) {
+		console.log('Data: ' + data);
+		console.log('Status: ' + textStatus);
+		console.log('Error: ' + errorThrown);
+		$("#posts").html('Error while loading posts');
+		console.log('Exiting onError');
+	}
 };
-
-app.initialize();
